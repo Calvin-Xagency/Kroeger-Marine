8a203c578a69c0e91efa1d2798136cf805ded51c
Calvin-Xagency <Calvin@Xagency.io>
2021-10-19 12:49:33 -0400

    add files for gulp

---
+++ local-only-scripts.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function($){
+	$('img').each(function() {
+		var src = $(this).attr("src");
+		//var regex = /\/\/.*\/wp-content/; matches on ///wp-content/
+		var regex = /.*\/wp-content/;
+
+		if (src != "" && src != undefined) {
+			$(this).attr("src", src.replace(regex, "https://blueridgemountainclub.com/wp-content"));
+			//console.log($(this).attr("src"));
+		}
+	});
+	  
+	$('*[style*=background-image]').each(function() {
+		var src = $(this).attr("style");
+		var regex = /\/\/.*\/wp-content/;
+	
+		$(this).attr("style", src.replace(regex, "https://blueridgemountainclub.com/wp-content"));    
+	});
+});
